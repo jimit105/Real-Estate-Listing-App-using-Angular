adc65f8a39ea1c33ba1ae2997c276253787ba398
Jimit Dholakia <jimitd101@gmail.com>
2018-04-11 22:11:41 +0530

    Setting up the form template

---
+++ src/app/add-listing-form/add-listing-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddListingFormComponent } from './add-listing-form.component';
+
+describe('AddListingFormComponent', () => {
+  let component: AddListingFormComponent;
+  let fixture: ComponentFixture<AddListingFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddListingFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddListingFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
