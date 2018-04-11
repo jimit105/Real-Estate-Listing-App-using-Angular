0b6a9e8ae241a1f464cbacd89cf20c20e0c6a3f4
Jimit Dholakia <jimitd101@gmail.com>
2018-04-11 21:09:54 +0530

    Implementing an Interface

--- src/app/crib-card/crib-card.component.ts
+++ src/app/crib-card/crib-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Crib } from './../crib';
 
 @Component({
   selector: 'app-crib-card',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CribCardComponent implements OnInit {
 
-  @Input('crib') crib: any;
+  @Input('crib') crib: Crib;
 
   constructor() { }
 
